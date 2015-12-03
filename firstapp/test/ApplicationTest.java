2f3a29f52a9c6cd7b12bc7bf1f31096f7737156e
Alexander Barkume <alexbarkume@dyn-160-39-224-50.dyn.columbia.edu>
2015-12-03 02:51:15 -0500

    Completed SubmitBid Unit Test

--- firstapp/test/ApplicationTest.java
+++ firstapp/test/ApplicationTest.java
@@ -28,6 +28,11 @@ import java.util.Date;
 import static play.test.Helpers.*;
 import static org.junit.Assert.*;
 
+import play.db.Database;
+import play.db.Databases;
+import play.db.evolutions.*;
+import java.sql.Connection;
+import com.google.common.collect.ImmutableMap;
 
 /**
 *
@@ -36,6 +41,12 @@ import static org.junit.Assert.*;
 *
 */
 public class ApplicationTest {
+    Database database;
+
+    @Before
+    public void createDatabase() {
+        database = Databases.createFrom("com.mysql.jdbc.Driver","jdbc:mysql://dojo.ckxewtmfwxoa.us-west-2.rds.amazonaws.com:3306/paddyschema", ImmutableMap.of("user", "dojo","password", "dojomaster"));
+      }
 
     @Test
     public void simpleCheck() {
@@ -44,38 +55,70 @@ public class ApplicationTest {
     }
     
     @Test
-    public void submitBitTest(){ //should do this with in memory so don't change db???
-       // Form<controllers.Application.Index> indexForm = new Form(); 
-        //indexForm = indexForm.fill(new controllers.Application.Index());
-        Long bid=45L;
-        Long artId=2L;
-        controllers.Application.Index index= new controllers.Application.Index();
-        index.bid=bid;
-        index.artId=artId;
-        //controllers.Application.Index.artId=artId;
-        Form<controllers.Application.Index> indexForm=Form.form(controllers.Application.Index.class);
-        indexForm = indexForm.fill(index);
-        assertEquals((Long) indexForm.get().bid,(Long) 55L);
-        /*Artwork Art = get an artwork from DB (same as Form one)
-        Long originalBid=Art.highBid()
-        User originalUser=Art.userWithHighBid
-        User biddingUser= get a user from DB
+    public void submitBitTest(){
+        running(fakeApplication(), new Runnable() {
+            public void run() {
+                models.Auctions auction = new models.Auctions();
+                controllers.Application test=new controllers.Application();
+
+                models.Artworks art = models.Artworks.find.byId(1L);
+                Long originalBid=art.auction.currentBid;
+                Long originalBidCount=art.auction.bidCount;
+                models.Users originalHighBidder=art.auction.userWithHighBid;
+                
+                //models.Users newBidder=new models.Users();
+                //newBidder.username="TestingSubmitBid";
+                //newBidder.password="test";
+                //newBidder.email="test@test.com";
+                //newBidder.save();
+                models.Users newBidder=models.Users.findByEmail("test@test.com");
+                
+                controllers.Application.Index index= new controllers.Application.Index();
+                index.artId=art.artid;
+                Form<controllers.Application.Index> indexForm=Form.form(controllers.Application.Index.class);
+
                 //check bid below high bid
-                Long newBid = 5;
-                Form<Index> IndexForm= Somehow create form that sends bid
-                submitBid(Art,user,IndexForm);
-                assert that Art.highBid==orignalBid and Art.user==originalUser
+                index.bid=0L;
+                indexForm = indexForm.fill(index);
+        
+                test.submitBid(art,newBidder,indexForm);
+ 
+                assertEquals((Long) art.auction.currentBid,(Long) originalBid);
+                assertEquals((Long) art.auction.bidCount,(Long) originalBidCount);
+                assertEquals((models.Users) art.auction.userWithHighBid, (models.Users) originalHighBidder);
                 
-                // check bid equal to high bid
-                Same as above but newBid=originalBid;
                 
-                //check non-sensical bid (eg sloppy joe)
-                String bid="Sloppy Joe"
-                run it, should be error though? idk 
+                
+                //check bid equal to high bid
+                index.bid=originalBid;
+                indexForm = indexForm.fill(index);
+        
+                test.submitBid(art,newBidder,indexForm);
+ 
+                assertEquals((Long) art.auction.currentBid,(Long) originalBid);
+                assertEquals((Long) art.auction.bidCount,(Long) originalBidCount);
+                assertEquals((models.Users) art.auction.userWithHighBid, (models.Users) originalHighBidder);
+                
+                
+                
                 //check bid above high bid
-                Same as above but newBid=100000;
-                Assert biddingUser and bid the new artwork stuff*/
+                index.bid=originalBid + 25;
+                indexForm = indexForm.fill(index);
+        
+                test.submitBid(art,newBidder,indexForm);
+ 
+                assertEquals((Long) art.auction.currentBid,(Long) (originalBid+25));
+                assertEquals((Long) art.auction.bidCount,(Long) (originalBidCount+1));
+                assertEquals((models.Users) art.auction.userWithHighBid, (models.Users) newBidder);
                 
+
+                art.auction.userWithHighBid=originalHighBidder;
+                art.auction.bidCount=originalBidCount;
+                art.auction.userWithHighBid=originalHighBidder;
+                art.save();
+            }
+        });
+
     }
             
    // @Test
@@ -90,6 +133,5 @@ public class ApplicationTest {
  //       assertEquals("text/html", contentType(html));
 //        assertTrue(contentAsString(html).contains("Your new application is ready."));
 //    }
-
 
 }
